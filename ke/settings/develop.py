2a66d64215252c99bd5be85b4618f81ac08c9c1c
din Lat <dinlat@MacBook-Air-din.Dlink>
2022-02-09 16:43:04 +0300

    production settings

--- ke/settings/develop.py
+++ ke/settings/develop.py
@@ -1,16 +1,11 @@
 from ke.settings.base import *
 
-import os
-
-ALLOWED_HOSTS = ['http://keanalytics-env.eba-5ypwhrkj.us-east-1.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
 
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': os.environ['RDS_DB_NAME'],
-    'USER': os.environ['RDS_USERNAME'],
-    'PASSWORD': os.environ['RDS_PASSWORD'],
-    'HOST': os.environ['RDS_HOSTNAME'],
-    'PORT': os.environ['RDS_PORT'],
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
--- ke/settings/production.py
+++ ke/settings/production.py
@@ -1,9 +1,15 @@
 from ke.settings.base import *
 
 
+ALLOWED_HOSTS = ['http://keanalytics-env.eba-5ypwhrkj.us-east-1.elasticbeanstalk.com/']
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ['RDS_DB_NAME'],
+    'USER': os.environ['RDS_USERNAME'],
+    'PASSWORD': os.environ['RDS_PASSWORD'],
+    'HOST': os.environ['RDS_HOSTNAME'],
+    'PORT': os.environ['RDS_PORT'],
     }
 }
