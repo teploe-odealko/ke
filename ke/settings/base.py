42c313174295b1179966c2511ebd241be5e5abba
din Lat <dinlat@MacBook-Air-din.Dlink>
2022-02-09 17:20:19 +0300

    psycopg2 to req.txt

--- ke/settings/base.py
+++ ke/settings/base.py
@@ -15,11 +15,10 @@ import os
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
