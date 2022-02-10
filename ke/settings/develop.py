abe28596f802fd29c308c455c9e5f78d1b238760
din Lat <dinlat@MacBook-Air-din.Dlink>
2022-02-10 19:36:11 +0300

    mysql init

--- ke/settings/develop.py
+++ ke/settings/develop.py
@@ -4,8 +4,12 @@ ALLOWED_HOSTS = ["*"]
 DEBUG = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+    'ENGINE': os.environ['RDS_DB_ENGINE'],
+    'NAME': os.environ['RDS_DB_NAME'],
+    'USER': os.environ['RDS_USERNAME'],
+    'PASSWORD': os.environ['RDS_PASSWORD'],
+    'HOST': os.environ['RDS_HOSTNAME'],
+    'PORT': os.environ['RDS_PORT'],
     }
 }
